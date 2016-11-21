7e7b6284f15193c93f70a423baac54b577259c47
Haruka Kai <plalal@gmail.com>
2016-11-22 02:04:30 +0900

    parse-file

--- lib/text/znclog/parser.scm
+++ lib/text/znclog/parser.scm
@@ -1,27 +1,72 @@
 (define-module text.znclog.parser
+
   (export
+    default-path-of
     parse-line
-    ))
+    parse-file$
+    parse-file
+    )
+
+  (use util.match)
+  (use file.util)
+
+  )
 
 (select-module text.znclog.parser)
 
 (define (parse-line line)
   (rxmatch-case line
-    [ #/^\[(\d\d):(\d\d):(\d\d)\] (.*)$/ ( #f hh mm ss line )
+    [ #/^\[(\d\d):(\d\d):(\d\d)\] (.*)$/ ( #f HH MM SS line )
       (rxmatch-case line
         [ #/^\*\*\* (.*)$/ ( #f msg )
-          (values hh mm ss 'server-message msg)
+          (list HH MM SS
+                'server-message
+                msg)
           ]
         [ #/^<([^ ]+)> (.*)$/ ( #f nick msg )
-          (values hh mm ss 'user-message nick msg)
+          (list HH MM SS
+                'user-message
+                msg nick)
           ]
         [ else
           ;; unknown message type
-          (values hh mm ss #f line)
+          (list HH MM SS
+                #f
+                line)
           ]
         ) ]
     [ else
       ;; unknown line type
-      (values #f #f #f #f line)
+      (list #f #f #f
+            #f
+            line)
       ]
     ) )
+
+(define (default-path-of logroot nw ch yyyy mm dd)
+  ($ build-path logroot
+     $ format "~a_~a_~4,'0d~2,'0d~2,'0d.log"
+     nw ch yyyy mm dd
+     ) )
+
+(define ((parse-file$ path-of) logroot nw ch yyyy mm dd)
+  ($ reverse $ (cut vector-ref <> 1)
+     $ call-with-input-file (path-of logroot nw ch yyyy mm dd)
+     (^ (in)
+        (port-fold
+          (match-lambda*
+            [ ( line #( line-no parsed-lines ) )
+             (vector
+               (+ line-no 1)
+               (cons (match (parse-line line)
+                       [ ( HH MM SS type . args )
+                        `( ,nw ,ch ,yyyy ,mm ,dd ,HH ,MM ,SS ,line-no ,type ,@args )
+                        ] )
+                     parsed-lines) )
+             ]
+            )
+          '#( 0 () )
+          (pa$ read-line in)
+          ) ) ) )
+
+(define parse-file (parse-file$ default-path-of))
